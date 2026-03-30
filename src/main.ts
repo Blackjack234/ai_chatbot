627d5fb1865dab4c7f4c062598ad30cf0497d12c
Blackjack234 <paulanirban390@gmail.com>
2026-03-31 02:00:04 +0530

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe())
+
+  
   await app.listen(process.env.PORT ?? 3500,()=>{
     Logger.log(`Server is running on port http://localhost:${process.env.PORT ?? 3500}`)
   });
